68ee551a66866a7711f6a426e3ff53805ed6b782
Guido W Schmidt <g,w,schmidt@rinkon.ch>
2020-03-04 12:25:50 +0100

    Fix db-3.0 build

--- docker-build.sh
+++ docker-build.sh
@@ -5,7 +5,7 @@
 docker build -t gwsch/cmdbuild:app-3.2 3.2/cmdbuild/. --label "version=1.0" --label "maintaner=gwsch <g.w.schmidt@rinkon.ch>"
 docker build -t gwsch/cmdbuild:app-3.1.1 3.1.1/cmdbuild/. --label "version=1.0" --label "maintaner=gwsch <g.w.schmidt@rinkon.ch>"
 docker build -t gwsch/cmdbuild:app-3.1 3.1/cmdbuild/. --label "version=1.0" --label "maintaner=gwsch <g.w.schmidt@rinkon.ch>"
-docker build -t gwsch/cmdbuild:db-3.0 3.0/postgres/. --label "version=1.0" --label "maintaner=gwsch <g.w.schmidt@rinkon.ch>"
+docker build -t gwsch/cmdbuild:db-3.0 3.0/cmdbuild/. --label "version=1.0" --label "maintaner=gwsch <g.w.schmidt@rinkon.ch>"
 
 # ready2use
 docker build -t gwsch/cmdbuild:r2u-2.0-3.2 ready2use-2.0-3.2/. --label "version=1.0" --label "maintaner=gwsch <g.w.schmidt@rinkon.ch>"
